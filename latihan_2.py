a9d647ee2830dd5461aa15924e69b3701e83300e
andikasatria123 <125232148+andikasatria123@users.noreply.github.com>
2023-02-13 09:55:20 +0700

    Add files via upload

---
+++ latihan_2.py
@@ -0,0 +1,16 @@
+'''buatlah program menghitung luas bangunan datar menggunakan fungsi
+- segitiga
+- persegi panjang
+'''
+
+def segitiga(alas,tinggi):
+    luas = alas * tinggi / 2
+    print("luas segitiga adalah:%i cm"%luas)
+
+segitiga(30, 40)
+
+def segitiga_panjang(panjang_lebar):
+    luas = panjang * panjang_lebar
+    print("luas persegi panjang adalah:%i cm"%luas)
+
+persegi_panjang(60,1000)    
---
+++ latihan_fungsi.py
@@ -0,0 +1,40 @@
+'''fungsi pada python di definisikan dengan def'''
+
+'''def nama_fungsi():'''
+
+def salam():
+    print("assalamu'alaikum !! akhi ukhty")
+
+salam()
+salam()
+
+'''VERSI SAYA'''
+
+def persija():
+    print("the jak mania")
+
+persija()
+persija() 
+
+'''def nama_fungsi(p1, p2)'''
+
+def say_hi(nama):
+    print("aku adalah boboyboi", nama)
+
+say_hi("merah")
+say_hi("pink")
+say_hi("ijo")
+
+'''buatlah fungsi dengan 3 parameter'''
+
+def perkenalan(nama, alamat, umur):
+    print("nama saya:", nama)
+    print("alamat saya:", alamat)
+    print("umur saya:", umur, 'tahun')
+
+perkenalan("BU puan", "jakarta", 224)
+
+print("\n\n<========== BATAS ==========>\n\n")
+
+perkenalan("manto", "depok", 21)
+    
